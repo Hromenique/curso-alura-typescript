7bb0d28468044d8ea4fedacb9156840c7c582f6d
Hromenique Batista <hromenique.batista@maplink.global>
2018-03-23 09:31:47 -0300

    após aula 01 atividade 07

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -4,15 +4,19 @@ class Negociacao {
         this._quantidade = quantidade;
         this._valor = valor;
     }
+
     get data() {
         return this._data;
     }
+
     get quantidade() {
         return this._quantidade;
     }
+
     get valor() {
         return ths._valor;
     }
+
     get volume() {
         return this._quantidade * this._valor;
     }
