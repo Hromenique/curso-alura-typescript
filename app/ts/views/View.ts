a268a520a97675c2640952e6e2c0c8ba693d9f9e
Hromenique Batista <hromenique.batista@maplink.global>
2018-04-16 09:28:58 -0300

    fim aula 04

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,7 +1,13 @@
-class View {
-    protected _elemento: Element;
+abstract class View<T> {
+    private _elemento: Element;
 
     constructor(selector: string) {
         this._elemento = document.querySelector(selector);
     }
+
+    update(model: T){
+        this._elemento.innerHTML = this.template(model);
+    }
+
+    abstract template(model: T): string;
 }
