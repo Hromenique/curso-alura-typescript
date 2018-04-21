2499becff0e3d1d93a8604afc7b5a41f33132cea
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2018-04-21 16:33:32 -0300

    fim aula 02 atividade 03 - parâmetros opcionais

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,8 +1,8 @@
 export class Negociacao {    
     
-    constructor(readonly _data: Date, readonly _quantidade: number, readonly _valor: number){}    
+    constructor(readonly data: Date, readonly quantidade: number, readonly valor: number){}    
 
     get volume() {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 }
