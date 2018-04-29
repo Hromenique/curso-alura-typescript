828f1d763bec82445c3de4239ad367920b4f9a2e
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2018-04-28 23:40:47 -0300

    aula 04 atividade 02 - consumindo api externa

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -3,4 +3,5 @@ import { NegociacaoController } from './controllers/NegociacaoController';
 let controller = new NegociacaoController();
 
 $("form").submit(controller.adiciona.bind(controller));
+$('#botao-importa').click(controller.importarDados.bind(controller)); 
 
