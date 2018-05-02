88a55abba8bb56e7a2d69835084fc03b0e3842c0
Hromenique Batista <hromenique.batista@maplink.global>
2018-05-02 11:33:13 -0300

    aula 05 atividade 05: evitando importar duplicatas com interface

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -51,18 +51,20 @@ export class NegociacaoController {
 
     @throttle()
     importarDados() {
-
-        const isOk: ResponseHandler = (res: Response) => {
-            if (res.ok)
-                return res;
-
-            throw new Error(res.statusText);
-        }
-
         this._service
-            .obterNegociacoes(isOk)
-            .then(negociacoes => {
-                negociacoes.forEach(negociacao => this._negociacoes.adiciona(negociacao));
+            .obterNegociacoes(res => {
+                if (res.ok)
+                    return res;
+
+                throw new Error(res.statusText);
+            })
+            .then(negociacoesParaImportar => {
+                const negociacoesJaImportadas = this._negociacoes.paraArray();
+
+                negociacoesParaImportar
+                    .filter(negociacao => !negociacoesJaImportadas.some(jaImportada => negociacao.ehIgual(jaImportada)))
+                    .forEach(negociacao => this._negociacoes.adiciona(negociacao));
+
                 this._negociacoesView.update(this._negociacoes);
             });
     }
